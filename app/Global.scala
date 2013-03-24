9ec0e02f217fa7b119056781e4d16030649f6d17
Andrew Ringler <ringler@gmail.com>
2013-03-23 22:00:55 -0400

    cleaned up globals slightly

--- app/Global.scala
+++ app/Global.scala
@@ -1,9 +1,6 @@
 import play.api._
 
 package object globals extends GlobalSettings {
-  var tmdbApiKeyOption = Play.current.configuration.getString("tmdb.apikey")
-  if(tmdbApiKeyOption.isEmpty || tmdbApiKeyOption.get == ""){
-    throw new IllegalStateException("You must set an environment variable with TMDB_KEY equal to your TheMovieDb API Key")
-  }
-  val tmdbApiKey = tmdbApiKeyOption.get
+  // play has already throw an exception if this key is missing
+  val tmdbApiKey = Play.current.configuration.getString("tmdb.apikey").get 
 }
