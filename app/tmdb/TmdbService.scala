f5f24049cf11cbbf89f83c7337754ded78f967de
Andrew Ringler <ringler@gmail.com>
2013-04-14 13:24:42 -0400

    now handling better tmdb response codes

--- app/tmdb/TmdbService.scala
+++ app/tmdb/TmdbService.scala
@@ -1,14 +1,11 @@
 package tmdb
 
 import java.util.Date
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.concurrent.future
-
 import com.google.common.cache.CacheBuilder
 import com.google.common.cache.CacheLoader
-
 import models.Movie
 import models.TmdbCast
 import models.TmdbMovie
@@ -20,6 +17,7 @@ import play.api.libs.functional.syntax.functionalCanBuildApplicative
 import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import play.api.libs.json.__
 import play.api.libs.ws.WS
+import play.api.http.Status
 
 object TmdbService {
   // Search
@@ -50,49 +48,54 @@ object TmdbService {
       WS.url("http://api.themoviedb.org/3/search/movie")
         .withQueryString(("query", s), ("api_key", Global.tmdbApiKey))
         .get().map { response =>
-          implicit val castReads = (
-            (__ \ "id").read[Long] ~
-            (__ \ "name").read[String] ~
-            (__ \ "character").readNullable[String] ~
-            (__ \ "order").read[Int] ~
-            (__ \ "profile_path").readNullable[String])(TmdbCast)
-          implicit val movieReads = (
-            (__ \ "title").read[String] ~
-            (__ \ "release_date").readNullable[Date] ~
-            (__ \ "id").read[Long] ~
-            (__ \ "poster_path").readNullable[String] ~
-            (__ \ "casts" \ "cast").readNullable[List[TmdbCast]].orElse((__ \ "casts").readNullable[List[TmdbCast]]))(TmdbMovie)
-          implicit val searchReads = (
-            (__ \ "total_results").read[Int] ~
-            (__ \ "results").read[List[TmdbMovie]])(Search)
+          if (response.status == Status.OK) {
+            implicit val castReads = (
+              (__ \ "id").read[Long] ~
+              (__ \ "name").read[String] ~
+              (__ \ "character").readNullable[String] ~
+              (__ \ "order").read[Int] ~
+              (__ \ "profile_path").readNullable[String])(TmdbCast)
+            implicit val movieReads = (
+              (__ \ "title").read[String] ~
+              (__ \ "release_date").readNullable[Date] ~
+              (__ \ "id").read[Long] ~
+              (__ \ "poster_path").readNullable[String] ~
+              (__ \ "casts" \ "cast").readNullable[List[TmdbCast]].orElse((__ \ "casts").readNullable[List[TmdbCast]]))(TmdbMovie)
+            implicit val searchReads = (
+              (__ \ "total_results").read[Int] ~
+              (__ \ "results").read[List[TmdbMovie]])(Search)
 
-          response.json.validate[Search].fold(
-            valid = (search => {
-              val tmdbSearchCacheTimeInSeconds = 60 * 60 //1-hour
-              Cache.set(cacheKey, search, tmdbSearchCacheTimeInSeconds)
-              val movies = toMovies(s, search.movies)
-              movies foreach {
-                case movie =>
-                  Akka.future {
-                    val fetchedMovieInfoFuture = fetchAndPopulateFullMovieInfo(movie)
-                    fetchedMovieInfoFuture onFailure {
-                      case e: Exception => Logger.error("Unable to fetch & populate full movie info for '" + movie.title + "' " + e)
-                    }
-                    fetchedMovieInfoFuture onSuccess {
-                      case result: Boolean if !result => Logger.error("Unknown issue trying to fetch and populate movie info")
-                    }
+            response.json.validate[Search].fold(
+              valid = (search => {
+                val tmdbSearchCacheTimeInSeconds = 60 * 60 //1-hour
+                Cache.set(cacheKey, search, tmdbSearchCacheTimeInSeconds)
+                val movies = toMovies(s, search.movies)
+                movies foreach {
+                  case movie =>
+                    Akka.future {
+                      val fetchedMovieInfoFuture = fetchAndPopulateFullMovieInfo(movie)
+                      fetchedMovieInfoFuture onFailure {
+                        case e: Exception => Logger.error("Unable to fetch & populate full movie info for '" + movie.title + "' " + e)
+                      }
+                      fetchedMovieInfoFuture onSuccess {
+                        case result: Boolean if !result => Logger.error("Unknown issue trying to fetch and populate movie info")
+                      }
 
-                  } onFailure {
-                    case e: Exception => Logger.error("Issue with async execution while trying to fetch and populate full movie infor for '" + movie.title + "' " + e)
-                  }
-              }
-              movies
-            }),
-            invalid = (e => {
-              Logger.debug(response.status + " (GET) " + response.body)
-              Logger.error("Invalid JSON during query '+s+'" + e.toString)
-              Nil
-            }))
+                    } onFailure {
+                      case e: Exception => Logger.error("Issue with async execution while trying to fetch and populate full movie infor for '" + movie.title + "' " + e)
+                    }
+                }
+                movies
+              }),
+              invalid = (e => {
+                Logger.debug(response.status + " (GET) " + response.body)
+                Logger.error("Invalid JSON during query '+s+'" + e.toString)
+                Nil
+              }))
+          } else {
+            Logger.error("GET " + response.status + " Problem trying to call tmdb " + response.body)
+            Nil
+          }
         }
     }
   }
@@ -121,36 +124,41 @@ object TmdbService {
       WS.url("http://api.themoviedb.org/3/movie/" + movie.tmdbId)
         .withQueryString(("append_to_response", "casts"), ("api_key", Global.tmdbApiKey))
         .get().map { response =>
-          implicit val castReads = (
-            (__ \ "id").read[Long] ~
-            (__ \ "name").read[String] ~
-            (__ \ "character").readNullable[String] ~
-            (__ \ "order").read[Int] ~
-            (__ \ "profile_path").readNullable[String])(TmdbCast)
-          implicit val movieReads = (
-            (__ \ "title").read[String] ~
-            (__ \ "release_date").readNullable[Date] ~
-            (__ \ "id").read[Long] ~
-            (__ \ "poster_path").readNullable[String] ~
-            (__ \ "casts" \ "cast").readNullable[List[TmdbCast]])(TmdbMovie)
+          if (response.status == Status.OK) {
+            implicit val castReads = (
+              (__ \ "id").read[Long] ~
+              (__ \ "name").read[String] ~
+              (__ \ "character").readNullable[String] ~
+              (__ \ "order").read[Int] ~
+              (__ \ "profile_path").readNullable[String])(TmdbCast)
+            implicit val movieReads = (
+              (__ \ "title").read[String] ~
+              (__ \ "release_date").readNullable[Date] ~
+              (__ \ "id").read[Long] ~
+              (__ \ "poster_path").readNullable[String] ~
+              (__ \ "casts" \ "cast").readNullable[List[TmdbCast]])(TmdbMovie)
 
-          response.json.validate[TmdbMovie].fold(
-            valid = (tmdbMovie => {
-              val tmdbMovieCacheTimeInSeconds = 60 * 60 * 3 //3-hour
-              Cache.set(cacheKey, tmdbMovie, tmdbMovieCacheTimeInSeconds)
-              if (tmdbMovie.releaseDate.isDefined && tmdbMovie.posterPath.isDefined && tmdbMovie.cast.isDefined) {
-                Movie.update(movie.id, tmdbMovie)
-                true
-              } else{
-            	  Logger.debug("SKIP "+tmdbMovie.title +" missing fields")
-            	  false
-              }
-            }),
-            invalid = (e => {
-              Logger.debug(response.status + " (GET) " + response.body)
-              Logger.error("Invalid JSON during query '+s+'" + e.toString)
-              false
-            }))
+            response.json.validate[TmdbMovie].fold(
+              valid = (tmdbMovie => {
+                val tmdbMovieCacheTimeInSeconds = 60 * 60 * 3 //3-hour
+                Cache.set(cacheKey, tmdbMovie, tmdbMovieCacheTimeInSeconds)
+                if (tmdbMovie.releaseDate.isDefined && tmdbMovie.posterPath.isDefined && tmdbMovie.cast.isDefined) {
+                  Movie.update(movie.id, tmdbMovie)
+                  true
+                } else {
+                  Logger.debug("SKIP " + tmdbMovie.title + " missing fields")
+                  false
+                }
+              }),
+              invalid = (e => {
+                Logger.debug(response.status + " (GET) " + response.body)
+                Logger.error("Invalid JSON during query '+s+'" + e.toString)
+                false
+              }))
+          } else {
+            Logger.error("GET " + response.status + " Problem trying to call tmdb " + response.body)
+            false
+          }
         }
     }
   }
